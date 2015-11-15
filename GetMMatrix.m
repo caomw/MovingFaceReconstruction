a0678a60501083b873bf7c70ae86c122f1c72614
Sai Ganesh <saiganeshb@gmail.com>
2015-11-15 13:20:00 -0500

    Move some code into GetMMatrix

--- GetMMatrix.m
+++ GetMMatrix.m
@@ -4,6 +4,10 @@ function [M] = GetMMatrix(dir_name, templateFiduc, locs)
 %   grayscale value.
 
 s = dir(sprintf('%s/*.jpg', dir_name));
+im = imread(sprintf('%s/%s', dir_name, s(1).name));
+I2 = MakeFrontalFace(im, templateFiduc, locs);
+p = size(I2, 1)*size(I2, 2);
+
 num_images = size(s, 1);
 M = zeros(num_images, p);
 for i = 1:num_images
--- testMakeFrontalFace.m
+++ testMakeFrontalFace.m
@@ -2,11 +2,6 @@ f = load('init_face_data1.mat');
 templateFiduc = f.templateFiduc;
 locs = f.locs;
 
-%im = imread('lfw/George_HW_Bush/George_HW_Bush_0011.jpg');
-im = imread('lfw/George_HW_Bush/George_HW_Bush_0001.jpg');
-I2 = MakeFrontalFace(im, templateFiduc, locs);
-p = size(I2, 1)*size(I2, 2);
-
 dir_name = 'lfw/George_HW_Bush';
 M = GetMMatrix(dir_name, templateFiduc, locs);
 save('GeorgeHWBush_M.mat', 'M')
