23124618c7ad10ced7fdc260f01d1861ad072ab2
Sai Ganesh <saiganeshb@gmail.com>
2015-11-22 13:38:46 -0500

    add debug code to show the fiduc points

--- testFrontal2.m
+++ testFrontal2.m
@@ -2,7 +2,11 @@ f = load('init_face_data1.mat');
 templateFiduc = f.templateFiduc;
 locs = f.locs;
 
-im = imread('lfw/George_HW_Bush/George_HW_Bush_0001.jpg');
+im = imread('lfw/George_HW_Bush/George_HW_Bush_0002.jpg');
+[DETS,PTS,DESCS] = getFiducial(im);
+DebugPoint(im, PTS);
+
+return;
 figure;
 imshow(im);
 I2 = MakeFrontalFace(im, templateFiduc, locs);
