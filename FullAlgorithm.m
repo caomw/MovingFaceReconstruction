6a8db91bf9e913fb61d5fe23d73054c45b7e1ad7
nrahul <rahulnallamothu@gmail.com>
2015-11-26 15:02:52 -0500

    small change

--- FullAlgorithm.m
+++ FullAlgorithm.m
@@ -3,7 +3,7 @@
 f = load('init_face_data1.mat');
 templateFiduc = f.templateFiduc;
 locs = f.locs;
-GetS_t;
+%GetS_t;
 dir_name = 'lfw/George_HW_Bush';
 
 %  im = imread('lfw/George_HW_Bush/George_HW_Bush_0003.jpg');
