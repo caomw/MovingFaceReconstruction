8f35393dab1b0c22cc8c263281b5721ffa6e6b26
nrahul <rahulnallamothu@gmail.com>
2015-11-26 14:38:54 -0500

    added locations mat file

--- DebugInitLightAndShape.m
+++ DebugInitLightAndShape.m
@@ -1,11 +1,11 @@
-for j = 1:10
+for j = 2:10
     L = L_all{j};
     S = S_all{j};
     M_rec = L*S;
 
 for i = 1:1%size(M_rec,1)
    
-    im = reshape(M_rec(i,:),sx,sy);
+    im = reshape(M_rec(i,:),sx,[]);
     imshow(im);
     
 end
