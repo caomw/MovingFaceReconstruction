9353f4bdd30dc792ad5fbd57f4c235460c0358f1
Sai Ganesh <saiganeshb@gmail.com>
2015-11-28 20:39:03 -0500

    Make M matrix for gw bush, 505 images

--- GetMMatrix.m
+++ GetMMatrix.m
@@ -9,12 +9,19 @@ p = size(I2, 1)*size(I2, 2);
 
 num_images = size(db_images, 1);
 M = zeros(num_images, p);
+tic
 for i = 1:num_images
     db = db_images{i};
     I2 = im2double(...
         rgb2gray(MakeFrontalFace(db.im, db.fiduc, templateFiduc, locs)));
     M(i, :) = reshape(I2, 1, []);
+    if(mod(i, 10) == 1)
+        i
+        toc
+        tic
+    end
 end
+toc
 
 end
 
