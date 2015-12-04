8f5b7f7ca1a68cab310905116a5e00448eb28e6e
Sai Ganesh <saiganeshb@gmail.com>
2015-12-03 21:34:58 -0500

    minor changes

--- collectionflow/morphingdemo.m
+++ collectionflow/morphingdemo.m
@@ -14,13 +14,13 @@ I1f = reshape(Mflat(:, 22), S_r, []);
 I2 = reshape(M(:, 67), S_r, []);
 I2f = reshape(Mflat(:, 67), S_r, []);
 
-ShowImages(I1, I1f);
-ShowImages(I2, I2f);
+ShowImages(I1, I1f, 'I1 and normalized');
+ShowImages(I2, I2f, 'I2 and normalized');
 
 [vx1, vy1, w1] = Coarse2FineTwoFrames(I1, I1f, para);
-[vx2, vy2, ~] = Coarse2FineTwoFrames(w1, I2, para);
+[vx2, vy2, ~] = Coarse2FineTwoFrames(I2f, I2, para);
 
-ShowImages(I1, w1);
+ShowImages(I1, w1, 'I1 to w1');
 
-I3 = WarpImage(I1, vx1 + vx2, vy1 + vy2);
-ShowImages(I1, I3);
+I3 = WarpImage(w1, vx1 +  vx2, vy1 + vy2);
+ShowImages(I1, I3, 'final');
