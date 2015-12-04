b0ce02bd1230aa1294bfa52d450e34b761d69b71
Sai Ganesh <saiganeshb@gmail.com>
2015-12-03 21:06:49 -0500

    add code for baseline morphing. Doesnt work

---
+++ collectionflow/ShowImages.m
@@ -0,0 +1,12 @@
+function [] = ShowImages(I1, I2)
+%UNTITLED73 Summary of this function goes here
+%   Detailed explanation goes here
+
+figure;
+subplot(1, 2, 1);
+imshow(I1);
+subplot(1, 2, 2);
+imshow(I2);
+
+end
+
