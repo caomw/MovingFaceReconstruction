7f1e469820d3fdb955643f41a1e8156f5ff1953d
Sai Ganesh <saiganeshb@gmail.com>
2015-12-05 11:22:08 -0500

    update demo image pair

--- collectionflow/emorph3.m
+++ collectionflow/emorph3.m
@@ -5,14 +5,16 @@ imsize = [150 116 3];
 para = get_default_cflow_para();
 end
 
-k1 = 25;
+k1 = 363;
 I1 = reshape(M(:, k1), imsize);
 I1f = reshape(Mflat(:, k1), imsize);
 
 
-k2 = 57;
+k2 = 261;
 I2 = reshape(M(:, k2), imsize);
 I2f = reshape(Mflat(:, k2), imsize);
 
 mv = MakeMorphVideoColor(I1, I1f, I2, I2f, 25);
+ShowImages(I1, I1f, 'im1');
+ShowImages(I2, I2f, 'im2');
 implay(mv);
